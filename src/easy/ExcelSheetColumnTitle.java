ca948b1c967400d1f91183c0ac1a5ad274ada507
Ankit Tare <atare@andrew.cmu.edu>
2016-03-01 00:28:35 -0500

    Easy 5
    Last 5 easy problems

---
+++ src/easy/ExcelSheetColumnTitle.java
@@ -0,0 +1,25 @@
+package easy;
+import java.util.*;
+public class ExcelSheetColumnTitle {
+
+    public String convertToTitle(int n) {
+	StringBuilder sb = new StringBuilder();
+	Map<Integer, Character> map = new HashMap<Integer, Character>();
+
+	for (int i = 1; i <= 26; i++)
+	    map.put(i, (char) (i + 64));
+	System.out.println(map);
+	while (n >= 1) {
+	    int r = n % 26;
+	    n = n / 26;
+	    if (r == 0) {
+		sb.append(map.get(26));
+		n--;
+	    } else {
+		sb.append(map.get(r));
+	    }
+	}
+	return sb.reverse().toString();
+    }
+
+}
