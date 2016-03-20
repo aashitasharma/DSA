ef813e760b017db1a886d1ac43920598baf66407
Ankit Tare <atare@andrew.cmu.edu>
2016-03-20 00:54:50 -0400

    Medium Questions

---
+++ src/medium/SummaryRanges.java
@@ -0,0 +1,37 @@
+package medium;
+import java.util.*;
+public class SummaryRanges {
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result=new LinkedList<String>();
+        StringBuilder sb=new StringBuilder();
+        boolean flag=false;
+        if(nums==null || nums.length==0) return result;
+        if(nums.length==1){
+            result.add(nums[0]+"");
+            return result;
+        }
+        for(int i=0;i<nums.length-1;i++){
+            if(-nums[i]+nums[i+1]==1){
+                if(flag==false){
+                    sb.append(nums[i]+"").append("->");
+                    flag=true;                
+                }
+            }
+            else{
+                sb.append(nums[i]+"");
+                result.add(sb.toString());
+                sb.delete(0, sb.length());
+                flag=false;
+            }
+        }
+        if(nums[nums.length-1]-nums[nums.length-2]==1){
+            sb.append(nums[nums.length-1]);
+            result.add(sb.toString());
+        }
+        else
+            result.add(nums[nums.length-1]+"");
+        return result;
+    }
+
+}
---
+++ src/medium/ThreeSum.java
@@ -0,0 +1,20 @@
+package medium;
+import java.util.*;
+public class ThreeSum {
+
+    public ThreeSum() {
+	// TODO Auto-generated constructor stub
+    }
+//
+//    public List<List<Integer>> threeSum(int[] a){
+//	Set<List<Integer>> set=new HashSet<List<Integer>>();
+//	int[] c=new int[a.length];
+//	int k=0;
+//	for(int i:a)
+//	    c[k++]=-i;
+//	Set<Integer> twosum=new HashSet<Integer>();
+//	for(int i:c){
+//	   
+//	}
+//    }
+}
