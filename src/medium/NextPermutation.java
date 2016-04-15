7d11d8c917743a05eb65c2b8392d95e8568ccd6a
Ankit Tare <atare@andrew.cmu.edu>
2016-04-15 02:56:51 -0400

    Next Permutation

--- src/medium/NextPermutation.java
+++ src/medium/NextPermutation.java
@@ -2,48 +2,53 @@ package medium;
 
 public class NextPermutation {
 
-    public NextPermutation() {
-	// TODO Auto-generated constructor stub
-    }
-    
-    public static void nextPermutation(int[] a) {
-	if(a==null || a.length==0)
-	    return ;
-	int p1=a.length-1;
-	int p2=p1-1;
-	while(p2>=0){
-	    if(a[p1]>a[p2]){
-		rotate(a, p2,p1);
-		return ;
-	    }
-	    p2--;
+	public NextPermutation() {
+
 	}
-	reverse(a);
-    } 
-    
-    public static void rotate(int[] a, int s, int e){
-	int t=a[e];
-	for(int i=e;i>=s;i--){
-	    a[i]=a[i-1];
+
+	public static void nextPermutation(int[] a) {
+		if (a == null || a.length == 0)
+			return;
+		int k = 0;
+		for (int i = a.length-1; i > 0; i--) {
+			if (a[i] > a[i - 1]) {
+				k = i-1;
+				break;
+			}
+		}
+		if (k == 0){
+			reverse(a, 0, a.length-1);
+			return;
+		}
+		int highest_index = 0;
+		for (int i = a.length-1; i > k; i--) {
+			if (a[i] > a[k]) {
+				highest_index = i;
+				break;
+			}
+		}
+		a[k] = a[highest_index] ^ a[k];
+		a[highest_index] = a[highest_index] ^ a[k];
+		a[k] = a[highest_index] ^ a[k];
+		reverse(a, k + 1, a.length-1);
 	}
-	a[s]=t;
-    }
-    
-    public static void reverse(int[] a){
-	int l=0;
-	int h=a.length-1;
-	while(l<h){
-	    a[l]=a[l]^a[h];
-	    a[h]=a[l]^a[h];
-	    a[l]=a[l]^a[h];
-	    l++;
-	    h--;
+
+	public static void reverse(int[] a, int start, int end) {
+		int l = start;
+		int h = end;
+		while (l < h) {
+			a[l] = a[l] ^ a[h];
+			a[h] = a[l] ^ a[h];
+			a[l] = a[l] ^ a[h];
+			l++;
+			h--;
+		}
 	}
-    }
-    public static void main(String[] args){
-	int[] a={1,1,5};
-	nextPermutation(a);
-	for(int i:a)
-	    System.out.print(i+" ");
-    }
+
+	public static void main(String[] args) {
+		int[] a = { 3, 2, 1 };
+		nextPermutation(a);
+		for (int i : a)
+			System.out.print(i + " ");
+	}
 }
